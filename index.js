bb3692f8a69c23b040742ff5227a54148bcdf8bd
rakeshbade <rakeshbade@gmail.com>
2024-02-19 03:31:11 +0000

    update all wires config

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const {fetchData, convertEntriesToRss} = require("./controllers/fetch")
+const { fetchData, convertEntriesToRss } = require("./controllers/fetch");
 const port = process.env.PORT || 3000; // You can choose any port number
 
-
-app.get('/', async (req, res) => {
-  const {blog, jsLoad} = req.query;
-  if(!blog) return res.status(404).send({message:"invalid query"});
-  try{
+app.get("/rss", async (req, res) => {
+  const { blog, jsLoad } = req.query;
+  if (!blog) return res.status(404).send({ message: "invalid query" });
+  try {
     const entries = await fetchData(blog, jsLoad);
     const xmlFeedWithEntries = convertEntriesToRss(blog, entries);
     return res.send(xmlFeedWithEntries);
-  }catch(e){
+  } catch (e) {
     return res.status(500).send(e);
   }
-
 });
 
 // Start the server
