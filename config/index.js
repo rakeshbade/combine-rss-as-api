b8cfbfda5c75ab3ee04470b9eb7923a987a81b1b
Rakesh Bade <rakeshbade@gmail.com>
2024-02-26 02:29:56 +0000

    update lean

--- config/index.js
+++ config/index.js
@@ -1,6 +1,8 @@
 const allwires = require("./allwires.json");
 const business = require("./business.json");
+const learn = require("./learn.json");
 module.exports = {
   allwires,
   business,
+  learn
 };
