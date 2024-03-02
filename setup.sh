8fbba22e5cd14d7f562dc9086d928534394fb109
rakeshbade <rakeshbade@gmail.com>
2024-03-01 23:10:15 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -31,7 +31,7 @@ curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
   | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
   | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
 
-ngrok config add-authtoken $AUTH_TOKEN
+ngrok config add-authtoken "$AUTH_TOKEN"
 
 ngrok http http://localhost:3000
 
