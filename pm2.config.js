8892c7c18df6a685f9d6a0fe840b00700e0e6e97
rakeshbade <rakeshbade@gmail.com>
2024-03-10 22:51:00 -0500

    Update pm2.config.js

--- pm2.config.js
+++ pm2.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'combine-rss-api', // Replace with your app name
-      script: 'index.js', // Replace with your entry script file
+      script: 'node index.js', // Replace with your entry script file
       instances: 'max', // Spawn as many instances as CPU cores
       max_memory_restart: '300M', // Restart if memory usage exceeds 300 megabytes
       cron_restart: '0 */30 * * *', // Restart every 30 minutes (on the hour)
