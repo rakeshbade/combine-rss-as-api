9d869898f039c860ef8d960979f7dcfb4ec7ad66
Rakesh Bade <rakeshbade@gmail.com>
2024-03-17 22:00:04 +0000

    refine allwires

--- services/fetch.js
+++ services/fetch.js
@@ -8,7 +8,7 @@ const parser = new xml2js.Parser();
 const { writeToFile } = require("../utils/fileUtils");
 const { convertEntriesToRss } = require("../utils/feed");
 const { applicationLogger: LOG } = require("./logger");
-const {isWithInHours, curlChildProcess} = require("./utils")
+const { isWithInHours, curlChildProcess } = require("./utils")
 
 
 const isPostFiltered = (post, blogName) => {
@@ -27,7 +27,7 @@ const loadDataBy = {
   axios: "axios",
   barrons: "barrons",
   accesswire: "accesswire",
-  reuters:"reuters"
+  reuters: "reuters"
 };
 
 const createRetryPromise = (asyncFunction, maxRetries = 3, delayMs = 100) => {
@@ -55,7 +55,7 @@ const createRetryPromise = (asyncFunction, maxRetries = 3, delayMs = 100) => {
 
 const parseFeed = (feed) => {
   return new Promise((resolve) => {
-    if(Array.isArray(feed)) return resolve(feed);
+    if (Array.isArray(feed)) return resolve(feed);
     parser.parseString(feed, (err, result) => {
       const entries =
         result?.["rss"]?.["channel"]?.[0]?.["item"] || result?.urlset?.url;
@@ -145,17 +145,21 @@ const waitFor = (timer) => {
   });
 };
 
-const fetchArticlesForBarrons = async (url, config)=>{
+const fetchArticlesForBarrons = async (url, config) => {
   const { data } = await axios.get(url);
   const pageSourceHTML = data.toString();
   return getArticlesFromHtml(pageSourceHTML, config);
 }
 
-const fetchArticlesForAccessWire = async(url, config)=>{
-  const curlCommand = `curl '${url}' \
+const fetchArticlesForAccessWire = async (url, config) => {
+  const curlCommand = `curl 'https://www.accesswire.com/public/newsroom' \
   -X 'POST' \
   -H 'authority: www.accesswire.com' \
-  -H 'cookie: __ctmid=65d04d6f00028b352f9d0bf0; __ctmid=65d04d6f00028b352f9d0bf0; _gcl_au=1.1.232772572.1708150128; hubspotutk=b364064a5af9f38d6d9d86b78837df1e; messagesUtk=54cbfc1540144bfba56ba6fdec914ee5; .AspNetCore.Antiforgery.yHDs6zQR-IY=CfDJ8MSc_ezAjklJkECDjjBas-9I-4sc_d9AJqdUqiTHogN2KlOLRgS0gEEfnKZVh1L7chBd3DdejhWS6Q9o4ymBTuaJeXBZwl_g16QhX-VXPFAhLwKFh9G1GSbd65xtyoJAvnnuYRZfT83--rJ5jjrWrYs; _gid=GA1.2.1336151060.1709741404; _gat=1; ads__landing_url__c=https%3A%2F%2Fwww.accesswire.com%2Fnewsroom; ads__referral_url__c=https%3A%2F%2Fwww.accesswire.com%2Fnewsroom; _ga_K04P53QQQB=GS1.2.1709741404.11.0.1709741404.60.0.0; ads__geoip=country%3DUnited%2520States%26city%3DDallas%26latitude%3D32.7831%26longitude%3D-96.8067; __hstc=117828950.b364064a5af9f38d6d9d86b78837df1e.1708150128794.1709334624879.1709741404864.11; __hssrc=1; __hssc=117828950.1.1709741404864; _ga_LZSJWCFSJY=GS1.1.1709741404.13.0.1709741417.47.0.0; _ga=GA1.2.1058342375.1708150128' \
+  -H 'accept: */*' \
+  -H 'accept-language: en-US,en;q=0.9' \
+  -H 'account: 1' \
+  -H 'content-length: 0' \
+  -H 'cookie: __ctmid=65d04d6f00028b352f9d0bf0; __ctmid=65d04d6f00028b352f9d0bf0; _gcl_au=1.1.232772572.1708150128; hubspotutk=b364064a5af9f38d6d9d86b78837df1e; messagesUtk=54cbfc1540144bfba56ba6fdec914ee5; ads__landing_url__c=https%3A%2F%2Fwww.accesswire.com%2Fnewsroom; ads__referral_url__c=https%3A%2F%2Fwww.accesswire.com%2Fnewsroom; __hssrc=1; .AspNetCore.Antiforgery.yHDs6zQR-IY=CfDJ8Lh9PTP6ATdHgVQiR28b-92TTL15rceBx_5TWRNJDXUWnXIIoMYyaSp4Ei-6VrGRWkZbmr563JXK27t_Hcf2imMToDxLhVdIiTnF6r12pQeHGDjkS8gbRR0gMV0j5l6CgjJsnMSxcjZiLSkLHDCm-ww; _gid=GA1.2.1964757776.1710710224; ads__geoip=country%3DUnited%2520States%26city%3DAustin%26latitude%3D30.2428%26longitude%3D-97.7658; __hstc=117828950.b364064a5af9f38d6d9d86b78837df1e.1708150128794.1710259896807.1710710224238.14; _gat=1; _ga=GA1.1.1058342375.1708150128; _ga_K04P53QQQB=GS1.2.1710710223.14.1.1710710780.60.0.0; __hssc=117828950.2.1710710224238; _ga_LZSJWCFSJY=GS1.1.1710710223.16.1.1710710792.47.0.0' \
   -H 'origin: https://www.accesswire.com' \
   -H 'referer: https://www.accesswire.com/newsroom' \
   -H 'sec-ch-ua: "Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"' \
@@ -164,16 +168,30 @@ const fetchArticlesForAccessWire = async(url, config)=>{
   -H 'sec-fetch-dest: empty' \
   -H 'sec-fetch-mode: cors' \
   -H 'sec-fetch-site: same-origin' \
-  -H 'x-csrf-token-headername: CfDJ8MSc_ezAjklJkECDjjBas--_U-RcXf1xbs3E-xl28SLXn3yLSuRLkdsAps0m7f461gAru8F-ObY9ZhW6Eb6VftVnr5kHMVTJS52IDRL5M3DK6MnQS4EOIBK87u0ya3llKxip459V7EVY9WwlAaLml58'`
-  const data = await curlChildProcess(curlCommand)
-  const pageSourceHTML = data.toString();
-  return getArticlesFromHtml(pageSourceHTML, config);
+  -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36' \
+  -H 'x-csrf-token-headername: CfDJ8Lh9PTP6ATdHgVQiR28b-90p6GeoB1yXutfnTVJqJOrGUSzMeG-t2V7po9EbRLgcof7hOOzlXD-Bm29C7ipK47V15c6VRSbSigD2wWwbQgBhy396wmHndIJxvH0MfJpbyXUEjJIanbevTiQunx4twXE'`
+  let articles = [];
+  try {
+    const data = await curlChildProcess(curlCommand)
+    const jsonData = JSON.parse(data);
+    articles = (jsonData?.data?.articles || []).map((article)=>{
+      return {
+        title: article.title,
+        description: article.body.replace(/<\/?[^>]+(>|$)/g, ""),
+        date: article.adate || article.dateString,
+        link: article.releaseurl
+      }
+    })
+  } finally {
+    return articles;
+  }
+
 }
 
-const fetchArticlesForReuters = async (url, config)=>{
+const fetchArticlesForReuters = async (url, config) => {
   const { data } = await axios.get(url);
-  return (data?.result?.articles || []).map((post)=>{
-    post.date = post.published_time; 
+  return (data?.result?.articles || []).map((post) => {
+    post.date = post.published_time;
     post.link = `https://www.reuters.com${post.canonical_url}`
     return post
   })
@@ -187,26 +205,25 @@ const fetchEntries = async (blogName) => {
     if (rss["loadType"]) {
       currentLoadType = rss["loadType"];
     }
-
     const url = rss["blogUrl"];
     LOG.info(
       "Fetching data for " +
-        blogName +
-        " from " +
-        url +
-        " using load type " +
-        currentLoadType,
+      blogName +
+      " from " +
+      url +
+      " using load type " +
+      currentLoadType,
     );
     try {
       if (currentLoadType === loadDataBy.barrons) {
         return await fetchArticlesForBarrons(url, rss);
       } else if (currentLoadType === loadDataBy.puppeteer) {
         return await fetchArticlesFromPuppeter(url, rss);
-      }else if(currentLoadType === loadDataBy.accesswire){
+      } else if (currentLoadType === loadDataBy.accesswire) {
         return await fetchArticlesForAccessWire(url, rss);
-      }else if(currentLoadType === loadDataBy.reuters){
+      } else if (currentLoadType === loadDataBy.reuters) {
         return await fetchArticlesForReuters(url, rss);
-      }else {
+      } else {
         const { data } = await axios.get(url);
         return data;
       }
