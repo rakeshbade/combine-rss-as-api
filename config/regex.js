d5ea1f12199993270e26b0394baf7dfb82d7db1e
Rakesh Bade <rakeshbade@gmail.com>
2024-03-23 00:28:15 +0000

    update regex

--- config/regex.js
+++ config/regex.js
@@ -1,7 +1,7 @@
 module.exports = {
   allwires: {
     include: /(?:NASDAQ|NYSE)/i,
-    exclude: /(?:DEADLINE ALERT:|INVESTIGATION ALERT:|INVESTOR ALERT:|DEADLINE REMINDER:|SHAREHOLDER ALERT:|SPECIAL ALERT:|Buyout Alert:|SHAREHOLDER UPDATE:|INVESTOR DEADLINE|Securities Class Action|Class Action Lawsuit|Securities Fraud Lawsuit|BOARD OF DIRECTORS|Executive Vice President|Chief Executive Officer)/i
+    exclude: /(?:DEADLINE ALERT:|SPHERE ANNOUNCES FIRST KEYNOTE|INVESTIGATION ALERT:|INVESTOR ALERT:|DEADLINE REMINDER:|SHAREHOLDER ALERT:|SPECIAL ALERT:|Buyout Alert:|SHAREHOLDER UPDATE:|INVESTOR DEADLINE|SHAREHOLDER ACTION REMINDER|CLASS ACTION NOTICE:|Securities Class Action|Securities Fraud Class Action|Securities Fraud Lawsuit|Class Action Against|Class Action Lawsuit|Class Action Suit|Class Action Filed Against|BOARD OF DIRECTORS|Executive Vice President|Chief Executive Officer|Chief Communications Officer|Chief Operating Officer|Senior Notes Due 20|Earnings Conference Call on|Financial Results Conference Call on|Earnings Investor Call on|Investors Should Contact|Securities Law Violations|Shareholders that lost money|Investors who lost money|Notifies Shareholders of)/i
   },
     wsj: {
       include: /(?:\/deals\/)/i,
